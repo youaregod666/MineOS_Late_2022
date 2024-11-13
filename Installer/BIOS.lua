9d0c45f5a519174919aa9f9ca4feb0501274b669
youaregod666 <94928206+youaregod666@users.noreply.github.com>
2024-11-13 21:42:01 +0100

    Update BIOS.lua

--- Installer/BIOS.lua
+++ Installer/BIOS.lua
@@ -2,7 +2,7 @@
 local result, reason = ""
 
 do
-	local handle, chunk = component.proxy(component.list("internet")() or error("Required internet component is missing")).request("https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Installer/Main.lua")
+	local handle, chunk = component.proxy(component.list("internet")() or error("Required internet component is missing")).request("https://raw.githubusercontent.com/youaregod666/MineOS_Late_2022/refs/heads/Installer_Test/Installer/Main.lua")
 
 	while true do
 		chunk = handle.read(math.huge)
